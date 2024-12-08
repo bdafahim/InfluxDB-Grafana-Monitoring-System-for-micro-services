1efe69369ed1f3a986e067a34e190fbdab58889b
bdafahim <bdafahim@gmail.com>
2024-12-08 12:11:32 +0200

    4 services communication done successfully

--- top-n-universities-service/top-n-university-service.py
+++ top-n-universities-service/top-n-university-service.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, request
 import os
 import requests
-from prometheus_client import Counter, generate_latest
-from prometheus_client import start_http_server
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -10,7 +8,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 app = Flask(__name__)
 
 # Environment variables for InfluxDB configuration
-INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://localhost:8086")
+INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://http://influxdb-service-sdx-assignment-ahmedbad.2.rahtiapp.fi:8086")
 INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "n8oq7g0atJjfvnGVrFjSIOecH2qq20DyNDtanHpp5JQnChGsLJYpaRvx5nXqCZc3QrntRxePjcy9zMCPzt4_lQ==")
 INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "my-org")
 INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "my-bucket")
@@ -18,8 +16,7 @@ INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "my-bucket")
 # Initialize InfluxDB client
 client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 write_api = client.write_api(write_options=SYNCHRONOUS)
-# Prometheus metrics
-#REQUEST_COUNT = Counter("top_universities_requests_total", "Total requests to top-universities", ['endpoint'])
+
 
 RANKING_UPLOADER_URL = os.getenv("RANKING_UPLOADER_URL", "http://ranking-uploader:4200/rankings")
 
@@ -28,7 +25,6 @@ DEFAULT_TOP_N = int(os.getenv("TOP_N", 5))
 @app.route("/top", methods=["GET"])
 def get_top_universities():
     """Fetch top N universities from rankings."""
-    #REQUEST_COUNT.labels(endpoint="/top").inc()
     response = requests.get(RANKING_UPLOADER_URL)
     rankings = response.json()
 
@@ -55,12 +51,6 @@ def get_top_universities():
     return jsonify(top_universities)
 
 
-'''@app.route("/metrics", methods=["GET"])
-def metrics():
-    """Expose Prometheus metrics."""
-    return generate_latest(), 200'''
 
 if __name__ == "__main__":
-    # Start Prometheus metrics server on 8001
-    #start_http_server(8001)
     app.run(host="0.0.0.0", port=4201)
